c27b67a204d05fe574c736efa8ed7c1abbbe1d66
s-caluk <seher.caluk@gmail.com>
2022-11-17 00:06:39 +0100

    day01

---
+++ src/day01_programlamayaGiris/C01_HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+java OOP bir dildir.Nesne(Object) olusturmak icin Class(sablon) olusturulmak zorundadir.
+Her Class´da main methodu, variables, methodlar bulunur.
+Primitive DataTypes: boolean, char, float, double, byte, short, int, long
+Non-Primitive DataTypes: Arrays, Strings, Classes, Interfaces, Enums, Objects...
+Java hafizadaki datalari "variable" veya "obje"  yardimiyla kullanir.
+ */
+
+
+package day01_programlamayaGiris;
+
+public class C01_HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        // println  yazdirdiktan sonra alt satira gecer
+
+        System.out.print("Bugun kodlamada ilk gunumuz");
+        //sadece print yazarsak alt satira gecmez
+
+        System.out.println("Bu da 3.satir olsun");
+    }
+}
+
---
+++ src/day01_programlamayaGiris/C02_Variables.java
@@ -0,0 +1,31 @@
+
+/*
+int sayi; dediginde hafizada alan acar, ama kutunun ici bos.
+sayi = 20; dediginde kutunun icine degeri koyar. = assignment: deger atama.
+*/
+
+package day01_programlamayaGiris;
+
+public class C02_Variables {
+
+    public static void main(String[] args) {
+
+        int sayi=20;
+
+        System.out.println(sayi); // 20
+
+        System.out.println("sayi"); // sayi
+
+        sayi=30;
+
+        System.out.println(sayi); // 30
+
+        sayi= sayi + 20;
+
+        System.out.println(sayi); // 50
+
+    }
+
+}
+
+
