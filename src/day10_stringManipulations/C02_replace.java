fbbe5f69325af8547d59ac111680001373a6aa07
s-caluk <seher.caluk@gmail.com>
2022-11-28 23:30:00 +0100

    C03

--- src/day10_stringManipulations/C02_replace.java
+++ src/day10_stringManipulations/C02_replace.java
@@ -1,2 +1,39 @@
-package day10_stringManipulations;public class C02_replace {
+
+//replace bir String metodudur
+
+package day10_stringManipulations;
+
+public class C02_replace {
+    public static void main(String[] args) {
+
+        String str = "Java ogren, adana ye :)";
+
+        System.out.println(str.replace("a", "A"));
+        System.out.println(str.replace(" ", ""));
+        System.out.println(str.replace("Java", "Yazilim"));
+
+        String telefon = "555 34512346788";
+        System.out.println(telefon.replace("5", "22"));
+
+        //asagidaki arama sonucunun 10.000 den fazla oldugunu test edin
+
+        String sonuc = "1-16 of over 100,000 results for \"apple\"";
+        int indexOver = sonuc.indexOf("over");
+        int indexResult = sonuc.indexOf("result");
+
+        String sonucSayisi = sonuc.substring(indexOver+5, indexResult-1);
+        System.out.println(sonucSayisi); //100,000
+
+        sonucSayisi = sonucSayisi.replace(",","");
+        System.out.println(sonucSayisi);  //100000
+
+        int sonucSayisiInt = Integer.parseInt(sonucSayisi); //string 100000´i integere cevirdi
+
+        if (sonucSayisiInt>100000) {
+            System.out.println("arama sonuc testi PASSED");
+        }else{
+            System.out.println("arama sonuc testi FAILED");
+        }
+
+    }
 }
--- src/day10_stringManipulations/C03_replaceAll.java
+++ src/day10_stringManipulations/C03_replaceAll.java
@@ -1,2 +1,51 @@
-package day10_stringManipulations;public class C03_replaceAll {
+
+
+
+package day10_stringManipulations;
+
+public class C03_replaceAll {
+
+    public static void main(String[] args) {
+
+        //bu metindeki sayilardan kurtulun---------------------------------------------
+        String str= "J6a1va 566G1uz6,z1e6l-d61ir1";
+
+        System.out.println(str.replace("1","")
+                .replace("5","")
+                .replace("6","")); // Java Guz,zel-dir
+
+
+        //bunu nasil degistireceksin? tek tek zahmetli is------------------------------
+        str = "Ja7va,; og436ren,./dikce guzelle=-098siyor";
+        /*
+           replace() zaten istenen degisikligi verilen ozellige uyan tum degerler icin yapar
+           eger genellemeyi butun sayilari, butun space'leri, butun ozel karakterleri
+           gibi genisletmek istersek replace() yerine replaceAll() kullaniriz
+           Java bu genellemeleri yazabilmemiz icin regex(regular expressions) tanimlamistir
+           \\d : butun digit'ler
+         */
+
+        //sayilardan kurtariyor-------------------------------------------------------
+        str = str.replaceAll("\\d","");
+        System.out.println(str);                                      //Java,; ogren,./dikce guzelle=-siyor
+
+        //özel karakterlerden kurtul---------------------------------------------------
+        //sadece rakam ve harfler kalsin istiyorsan, disindakileri at:\\W
+        // space de ozel karakter oldugundan bu durumda space'in yok olmamasi icin
+        // once space yerine metinde olmayan bir deger atayalim
+
+        str = str.replace(" ", "5");
+        str = str.replaceAll("\\W","");
+        str = str.replace("5"," ");
+        System.out.println(str);
+
+        // buradaki harf sayisini bulun-----------------------------------------------
+        String input3="Ali Can, Merve Star, Mark Tom";
+
+        input3=input3.replaceAll("\\W","");
+        System.out.println(input3.length()); // 22
+
+
+
+    }
 }
