2d7233a6d82c7e8f5614d51b2a18d985918952e9
s-caluk <seher.caluk@gmail.com>
2022-11-28 01:02:03 +0100

    day05

--- src/day05_ifStatements/C01_IfStatements.java
+++ src/day05_ifStatements/C01_IfStatements.java
@@ -1,4 +1,41 @@
-package day05_ifStatements;
 
+/*
+JAva 3 lü sartlari kabul etmez: 0<not<50 gibi
+onun yerine 2 ye bölmen gerekyor : 0<not && not<50 gibi
+
+hangi kod bloguna uygunsa notunuz sadece orasi calir. diger if bloklari calismaz.
+ */
+
+package day05_ifStatements;
+import java.util.Scanner;
 public class C01_IfStatements {
+    public static void main(String[] args) {
+
+        // Ogrenciden notunu rakam olarak alip harf olarak yazdirin
+        // 0- 49,999 DD
+        // 50-64,999 CC
+        // 65-84,999 BB
+        // 85-100 AA
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen notunuzu giriniz");
+        double not= scan.nextDouble();
+
+        if (0<= not  && not <50){
+            System.out.println("Notunuz DD, maalesef kaldiniz");
+        }
+
+        if (50<=not && not<65 ){
+            System.out.println("Notunuz CC, tebrikler gectiniz");
+        }
+
+        if (65<=not && not<85){
+            System.out.println("Notunuz BB");
+        }
+
+        if (85<=not && not<=100){
+            System.out.println("Tebrikler, notunuz AA");
+        }
+
+    }
 }
--- src/day05_ifStatements/C03_IfStatements.java
+++ src/day05_ifStatements/C03_IfStatements.java
@@ -1,4 +1,15 @@
+
+
 package day05_ifStatements;
 
 public class C03_IfStatements {
+    public static void main(String[] args) {
+        String sonuc = "true"; //stringtir if sartina ekleyemezsin. illede boolean olmali
+        int snc=3;
+        boolean bl=true;
+
+        if (bl){
+            System.out.println("if den sonraki paranteze boolean disinda bir deger yazilamaz");
+        }
+    }
 }
