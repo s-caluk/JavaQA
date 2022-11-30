ed5dc7f0e49dd9e0514100267476b7eb8efe0e61
s-caluk <seher.caluk@gmail.com>
2022-11-30 20:10:19 +0100

    C09

--- src/day11_forLoops/C09_NestedLoop.java
+++ src/day11_forLoops/C09_NestedLoop.java
@@ -1,2 +1,31 @@
-package day11_forLoops;public class C09_NestedLoop {
+package day11_forLoops;
+
+public class C09_NestedLoop {
+    public static void main(String[] args) {
+
+        // input olarak verilen kenar uzunluklarina gore
+        // asagidaki sekli cizdirin
+
+        int kisaKenar=4;           //bu format ile dinamik oluyor
+        int uzunKenar=7;
+
+        /*
+                * * * * * * * *   // bir satiri nasil yazdiracagimizi inner loop ile belirliyoruz
+                * * * * * * * *
+                * * * * * * * *
+                * * * * * * * *
+                * * * * * * * *
+             Satirda yaptigimiz islemi kac kere tekrar edecegimizi ise outer loop belirliyor
+         */
+
+        for (int i = 1; i <=kisaKenar ; i++) {
+
+            for (int j = 1; j <=uzunKenar ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(""); // satir ile islemimiz bittiginde alt satira gecmesi icin
+        }
+
+
+    }
 }
