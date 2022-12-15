1b497eb2d9180da6d0bcdf7564f249d322ab214d
s-caluk <seher.caluk@gmail.com>
2022-12-15 15:08:44 +0100

    C09

--- src/day16_arrays/C08_Soru.java
+++ src/day16_arrays/C08_Soru.java
@@ -1,4 +1,31 @@
 package day16_arrays;
 
+import java.util.Arrays;
+
 public class C08_Soru {
+    public static void main(String[] args) {
+        // Verilen bir array’deki tum elementleri bir saga kaydirip,
+        // sondaki elementi de basa tasiyacak bir method olusturun,
+        // array’i yeni haliyle kaydedin.
+        //	Orn :  input : [4,5,6,7]   array’in son hali. : [7,4,5,6]
+
+        int[] arr= {4,5,6,7};
+        arr= sagaKaydir(arr);
+
+        System.out.println(Arrays.toString(arr)); // [7, 4, 5, 6]
+    }
+
+    public static int[] sagaKaydir(int[] arr){
+
+        int temp= arr[arr.length-1];  //önce bos kovaya son elemani tasiyor
+
+        for (int i = arr.length-1 ; i >=1 ; i--) {  //index 1 e kadar degis!
+
+            arr[i]=arr[i-1];
+        }
+
+        arr[0]=temp;     // sifirinci indexe son elemani atiyor
+
+        return arr;
+    }
 }
---
+++ src/day16_arrays/C12_sort.java
@@ -0,0 +1,4 @@
+package day16_arrays;
+
+public class C12_sort {
+}
