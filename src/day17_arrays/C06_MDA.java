142758ca980bbb0da8cb7efad91cad3935da1742
s-caluk <seher.caluk@gmail.com>
2022-12-16 22:57:49 +0100

    C06

--- src/day17_arrays/C06_MDA.java
+++ src/day17_arrays/C06_MDA.java
@@ -1,4 +1,34 @@
 package day17_arrays;
 
 public class C06_MDA {
+    public static void main(String[] args) {
+
+        // Verilen iki katli String bir array'de
+        // istenen metni iceren elementleri yazdiran bir method olusturun
+
+        String[][] arr = {{"Ali","Zafer"},{"Betul","Huseyin","Hasan"},{"Said"}};
+
+        String arananMetin="a";
+
+        istenenElementleriYazdir(arr, arananMetin);
+        istenenElementleriYazdir(arr, "e");
+        istenenElementleriYazdir(arr, "Ali");
+    }
+
+    public static void istenenElementleriYazdir(String[][] arr, String arananMetin){
+
+        //önce hep insan olarak baksaydim nasil bir yöntem izlerdim diye bak?
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length ; j++) {
+
+                if(arr[i][j].contains(arananMetin)){
+                    System.out.print(arr[i][j] + " ");
+                }
+
+            }
+        }
+        System.out.println("");   //burasi alt satira gecmesi icin
+    }
+
 }
