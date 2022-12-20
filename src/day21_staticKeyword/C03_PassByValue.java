633428b8183227ac0ec3b00b3506c24d91091f5a
s-caluk <seher.caluk@gmail.com>
2022-12-21 00:17:33 +0100

    C03

--- src/day21_staticKeyword/C03_PassByValue.java
+++ src/day21_staticKeyword/C03_PassByValue.java
@@ -1,4 +1,28 @@
 package day21_staticKeyword;
 
 public class C03_PassByValue {
+    public static void main(String[] args) {
+        int sayi = 20 ;
+        int s = 20 ;
+        System.out.println(s);
+        //int sayi = 30;
+        //String sayi = "Hasan";
+
+        // bir scope´da tanimlanan variable´in yeniden tanimlanma imkani yoktur
+        // data türünü degissen de yeniden tanimlanamiyor! aksi halde
+        // sayi variablesini kullanirken hangisini kullanancagi mechul olur
+
+        method1();  //bütün s´lerin birbirinden bagimsiz olduguna dikkat et
+    }
+
+    public static void method1(){
+        method2(5);
+        String s = "Hasan";
+        System.out.println(s.toLowerCase()); //hasan
+    }
+
+    public static void method2(int s){
+        s = s*s;
+        System.out.println(s);
+    }
 }
