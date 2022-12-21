a2886b875e897c77f9a0e025257277b794d90de5
s-caluk <seher.caluk@gmail.com>
2022-12-21 14:01:45 +0100

    C05

--- src/day21_staticKeyword/C05_PassByValueCokluElement.java
+++ src/day21_staticKeyword/C05_PassByValueCokluElement.java
@@ -1,4 +1,21 @@
+
 package day21_staticKeyword;
 
+import java.util.Arrays;
+
 public class C05_PassByValueCokluElement {
+    public static void main(String[] args) {
+
+        int[] arr = {3,6,9,1,7};
+        elementleriDegistir(arr);
+        System.out.println(Arrays.toString(arr)); //[13, 6, 19, 1, 7] coklu elementde degisiklik kalici oldu
+
+
+    }
+     public static void elementleriDegistir(int[] b ){
+        //arr ve b zeiger´leri heap memorydeki ayni wohnungu gösteriyor gibi düsün
+        b[0] = 13;
+        b[2] = 19;
+         System.out.println(Arrays.toString(b));   //[13, 6, 19, 1, 7]
+     }
 }
