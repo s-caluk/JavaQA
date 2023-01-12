e2beb750cc3ced8286d94290a3e3e82a9ad489af
s-caluk <seher.caluk@gmail.com>
2023-01-12 14:03:56 +0100

    day23

---
+++ src/day34_Maps_NestedMaps/C01_NestedOgrenciMap.java
@@ -0,0 +1,12 @@
+package day34_Maps_NestedMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C01_NestedOgrenciMap {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> ogrenciMap= new HashMap<>();
+    }
+}
---
+++ src/day34_Maps_NestedMaps/C02_NestedMap.java
@@ -0,0 +1,17 @@
+
+
+package day34_Maps_NestedMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C02_NestedMap {
+    public static void main(String[] args) {
+        Map<String, String> bookingdatesMap = new HashMap<>();
+        bookingdatesMap.get()
+
+    }
+
+
+
+}
