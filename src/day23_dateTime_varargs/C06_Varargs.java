2c935bf60ed93bfa93b0ca2fa13cdf247caa90c3
s-caluk <seher.caluk@gmail.com>
2023-01-13 00:33:46 +0100

    day23

--- src/day23_dateTime_varargs/C06_Varargs.java
+++ src/day23_dateTime_varargs/C06_Varargs.java
@@ -1,9 +1,41 @@
 
 /*
+bunu argüment ve parametre disinda kullanmak anlamsiz. bu bir array zaten.
 
  */
 
 package day23_dateTime_varargs;
 
 public class C06_Varargs {
+    public static void main(String[] args) {
+
+        // Oyle bir method olusturalim ki
+        // kactane argument yazarsam yazayim
+        // argumentlerin toplamini versin
+
+        topla(5,6);
+        topla(5,6,7);
+        topla(2,3,4,6,7,7,8);
+
+        /*
+            Varargs array altyapisini kullanir
+            Temel hedef argument sayisi degisse bile
+            ayni data turundeki tum argumentleri alabilecek bir parametre olusturmaktir.
+         */
+    }
+    private static void topla(int... sayilar) {
+
+        int toplam1 = 0;
+        for (int i = 0; i < sayilar.length ; i++) {
+            toplam1 += sayilar[i];
+        }
+        System.out.println(toplam1);
+
+        int toplam2 = 0;
+        for (int each:sayilar
+             ) {
+            toplam2 += each;
+        }
+        System.out.println(toplam2);
+    }
 }
